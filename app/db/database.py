ef65bd1171bbcca8d3e231fa848cca09e6b18489
IlmaxRehman <ilmarehman022@gmail.com>
2026-06-19 11:56:54 +0530

    Add environment-based database configuration

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlaichemy.orm import declarative_base, sessionmaker
+from app.core.config import DATABASE_URL
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/alemeno"
 
 engine = create_engine(DATABASE_URL)
 
