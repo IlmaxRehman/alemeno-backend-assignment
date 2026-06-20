0206a858dceb25afff92875e83545231a86c3845
IlmaxRehman <ilmarehman022@gmail.com>
2026-06-20 09:56:45 +0530

    Prepare application for Docker deployment

--- app/main.py
+++ app/main.py
@@ -2,7 +2,13 @@ from fastapi import FastAPI
 
 from app.api.jobs import router as jobs_router
 
+from app.db.database import Base, engine
+from app.models.job import Job
+from app.models.transaction import Transaction
+
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
--- app/worker/celery_app.py
+++ app/worker/celery_app.py
@@ -2,7 +2,7 @@ from celery import Celery
 
 celery_app = Celery(
     "alemeno",
-    broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/0",
+    broker="redis://redis:6379/0",
+    backend="redis://redis:6379/0",
     include=["app.worker.tasks"]
 )
